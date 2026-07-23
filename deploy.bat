a1cb655a6f3016ba514e1aa39bdf2e48549a97c8
Gary Blanchard <garyblanchard77@gmail.com>
2026-07-23 06:30:28 -0600

    Update site

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,7 @@
-copy /Y "C:\Users\garyb\Downloads\index.html" "C:\Users\garyb\kcwatchtrading\index.html" 
-git add index.html 
+@echo off 
+echo Deploying KC Watch Trading site... 
+if exist "C:\Users\garyb\Downloads\index_2.html" (copy /Y "C:\Users\garyb\Downloads\index_2.html" "C:\Users\garyb\kcwatchtrading\index.html") else if exist "C:\Users\garyb\Downloads\index.html" (copy /Y "C:\Users\garyb\Downloads\index.html" "C:\Users\garyb\kcwatchtrading\index.html") 
+git add -A 
 git commit -m "Update site" 
 git push 
+echo Done! Site will be live in 2-3 minutes. 
